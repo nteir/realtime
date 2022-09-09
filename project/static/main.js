e213a5e3a7507977615ee8992415a02cb5b5a7a6
Natalia Teplynina <nt@mnt9.com>
2022-09-09 12:25:00 +0300

    add graph

--- project/static/main.js
+++ project/static/main.js
@@ -1,6 +1,35 @@
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+var graphData = {
+    type: 'line',
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        datasets: [{
+            label: '',
+            data: [0, 0, 0, 0, 0, 0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {}
+};
+
+const myChart = new Chart(ctx, graphData);
+
+
 var socket = new WebSocket('ws://localhost/ws/int/');
 socket.onmessage = function(event){
-    var data = JSON.parse(event.data);
-    console.log(data);
-    document.querySelector('#msgtxt').innerHTML = data.message;
+    var djangoData = JSON.parse(event.data);
+    console.log(djangoData);
+
+    var newGraphData = graphData.data.datasets[0].data;
+    newGraphData.shift();
+    newGraphData.push(djangoData.message);
+    graphData.data.datasets[0].data = newGraphData;
+    myChart.update();
+
+    document.querySelector('#msgtxt').innerHTML = djangoData.message;
 }
